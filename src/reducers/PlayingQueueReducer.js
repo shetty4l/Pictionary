e90fe55cfaf9c260b24382e78cf4f9cf7ec550d5
shetty4l <suyashshetty29@gmail.com>
2018-08-12 18:18:36 -0700

    major refactor of PlayingQueueReducer.js

--- src/reducers/PlayingQueueReducer.js
+++ src/reducers/PlayingQueueReducer.js
@@ -1,41 +1,42 @@
 import { INIT_PLAYING_QUEUE, UPDATE_PLAYER } from '../actions/types'
 
-const INITIAL_STATE = []
+const INITIAL_STATE = { queue: [], wordsAlreadyAppeared: [] }
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case INIT_PLAYING_QUEUE: {
-      const teamMembers = action.payload
+      const { teamMembers, words } = action.payload
+      const wordsAlreadyAppeared = words.map(word => word.word)
+      let { teamA, teamB } = teamMembers
 
-      const { teamA, teamB } = teamMembers
+      let wordIndex = 0
+      for (let playerIndex = 0; playerIndex < teamA.length; ++playerIndex) {
+        teamA[playerIndex].word = words[wordIndex]
+        wordIndex++
+      }
+      for (let playerIndex = 0; playerIndex < teamB.length; ++playerIndex) {
+        teamB[playerIndex].word = words[wordIndex]
+        wordIndex++
+      }
 
       const iterLength = (teamA.length > teamB.length) ? teamA.length : teamB.length
 
-      let playingQueue = INITIAL_STATE
-      for (var i = 0; i < iterLength; ++i) {
+      let playingQueue = []
+      for (let i = 0; i < iterLength; ++i) {
         if (teamA[i]) playingQueue.push(teamA[i])
         if (teamB[i]) playingQueue.push(teamB[i])
       }
-
-      return playingQueue
+      console.log(playingQueue, wordsAlreadyAppeared)
+      return { ...state, queue: playingQueue, wordsAlreadyAppeared }
     }
     case UPDATE_PLAYER: {
-      const { category, result } = action.payload
-      let playingQueue = [ ...state ]
+      const { scoreEntry, word } = action.payload
+      let playingQueue = [ ...state.queue ]
       let player = playingQueue.shift()
-
-      if (result) {
-        let { solved } = player
-        let value = solved[category] + 1
-        player = { ...player, solved: { ...solved, [category]: value } }
-      } else {
-        let { failed } = player
-        let value = failed[category] + 1
-        player = { ...player, failed: { ...failed, [category]: value } }
-      }
-
+      player = { ...player, score: [...player.score, scoreEntry], word }
+      const wordsAlreadyAppeared = [ ...state.wordsAlreadyAppeared, word.word ]
       playingQueue.push(player)
-      return playingQueue
+      return { ...state, queue: playingQueue, wordsAlreadyAppeared }
     }
     default:
       return state
