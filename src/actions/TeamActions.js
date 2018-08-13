1ed9ac2dd0070990d889c70e3e753645d34146e4
shetty4l <suyashshetty29@gmail.com>
2018-08-12 18:17:11 -0700

    each player is associated with a word that is fetched in the playing queue

--- src/actions/TeamActions.js
+++ src/actions/TeamActions.js
@@ -4,7 +4,8 @@ import {
   TEAMS_UPDATED,
   TEAM_CHANGE,
   INIT_PLAYING_QUEUE,
-  UPDATE_PLAYER
+  UPDATE_PLAYER,
+  NEW_WORD_FIREBASE_URL
 } from './types'
 
 export const teamAUpdate = (name) => {
@@ -37,15 +38,30 @@ export const teamChange = (player) => {
 }
 
 export const initPlayingQueue = (teamMembers) => {
-  return {
-    type: INIT_PLAYING_QUEUE,
-    payload: teamMembers
+  return async (dispatch) => {
+    let promiseArray = []
+    const { teamA, teamB } = teamMembers
+    for (let i = 0; i < teamA.length + teamB.length; ++i) {
+      const body = { score: [], wordsAlreadyAppeared: [] }
+      promiseArray.push(fetch(NEW_WORD_FIREBASE_URL, { // eslint-disable-line no-undef
+        method: 'POST',
+        body: JSON.stringify(body)
+      }).then(response => response.json()))
+    }
+    const words = await Promise.all(promiseArray)
+    const payload = { teamMembers, words }
+    dispatch({ type: INIT_PLAYING_QUEUE, payload })
   }
 }
 
-export const updatePlayer = (category, result) => {
-  return {
-    type: UPDATE_PLAYER,
-    payload: { category, result }
+export const updatePlayer = (player, wordsAlreadyAppeared, scoreEntry) => {
+  return async (dispatch) => {
+    const { score } = player
+    const body = { score, wordsAlreadyAppeared }
+    let response = await fetch(NEW_WORD_FIREBASE_URL, { // eslint-disable-line no-undef
+      method: 'POST', body: JSON.stringify(body)
+    })
+    const word = await response.json()
+    dispatch({ type: UPDATE_PLAYER, payload: { scoreEntry, word } })
   }
 }
