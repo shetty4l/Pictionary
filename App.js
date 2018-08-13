30bf875a682926f3e01e06e18f38a8055b1bdcbb
shetty4l <suyashshetty29@gmail.com>
2018-08-13 14:52:29 -0700

    remove firebase connection in App.js

--- App.js
+++ App.js
@@ -2,22 +2,10 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import ReduxThunk from 'redux-thunk'
-import firebase from 'firebase'
 import reducers from './src/reducers'
 import Router from './src/Router'
 
 class App extends Component {
-  componentWillMount () {
-    const config = {
-      apiKey: 'AIzaSyBUssBEK5JZkSEWwm-DLfwk-eDho3fM6vQ',
-      authDomain: 'pictionary-add99.firebaseapp.com',
-      databaseURL: 'https://pictionary-add99.firebaseio.com',
-      projectId: 'pictionary-add99',
-      storageBucket: 'pictionary-add99.appspot.com',
-      messagingSenderId: '1056790261509'
-    }
-    firebase.initializeApp(config)
-  }
   render () {
     return (
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
